14af72ae82903be5200382aa69e69f79d70b7fc1
SkyZeroZx <73321943+SkyZeroZx@users.noreply.github.com>
2022-11-12 13:54:52 -0500

    Update Logger

--- src/config/mailer/mailer.ts
+++ src/config/mailer/mailer.ts
@@ -19,13 +19,13 @@ transporter
   .verify()
   .then(() => {
     logger.log('Servidor de correos inicializado');
-    console.log('Process DATABASE_HOST', process.env.DATABASE_HOST);
-    console.log('Process URL_WEB', process.env.URL_WEB);
-    console.log('Process APP_NAME', process.env.APP_NAME);
+    logger.error('Process APP_NAME', process.env.APP_NAME)
+    logger.error('Process DATABASE_HOST', process.env.DATABASE_HOST)
+    logger.error('Process URL_WEB', process.env.URL_WEB) 
   })
   .catch((error) => {
-    console.log('Process DATABASE_HOST', process.env.DATABASE_HOST);
-    console.log('Process URL_WEB', process.env.URL_WEB);
-    console.log('Process APP_NAME', process.env.APP_NAME);
+    logger.error('Process APP_NAME', process.env.APP_NAME)
+    logger.error('Process DATABASE_HOST', process.env.DATABASE_HOST)
+    logger.error('Process URL_WEB', process.env.URL_WEB) 
     logger.error('Sucedio un error al iniciarlizar el servidor de correos', error);
   });
