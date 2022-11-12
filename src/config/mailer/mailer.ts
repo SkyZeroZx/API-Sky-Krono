88120decfe6d810e95ac1c9621719963540431e4
SkyZeroZx <73321943+SkyZeroZx@users.noreply.github.com>
2022-11-12 13:52:42 -0500

    Update

--- src/config/mailer/mailer.ts
+++ src/config/mailer/mailer.ts
@@ -19,7 +19,13 @@ transporter
   .verify()
   .then(() => {
     logger.log('Servidor de correos inicializado');
+    console.log('Process DATABASE_HOST', process.env.DATABASE_HOST);
+    console.log('Process URL_WEB', process.env.URL_WEB);
+    console.log('Process APP_NAME', process.env.APP_NAME);
   })
   .catch((error) => {
+    console.log('Process DATABASE_HOST', process.env.DATABASE_HOST);
+    console.log('Process URL_WEB', process.env.URL_WEB);
+    console.log('Process APP_NAME', process.env.APP_NAME);
     logger.error('Sucedio un error al iniciarlizar el servidor de correos', error);
   });
